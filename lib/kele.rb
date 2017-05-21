e658e2be42955746313f0801b14c737a5d670972
Sajeev Ramasamy <thorion3006@gmail.com>
2017-05-21 16:49:06 +0000

    Retrive users: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,16 +1,23 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(username, password)
-    base_uri = 'https://www.bloc.io/api/v1'
-
     values = {
       email: username,
       password: password
     }
 
-    @auth_token = Kele.post(base_uri + '/sessions', body: values)
+    response = self.class.post('/sessions', body: values)
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { authorization: @auth_token })
+    JSON.parse(response.body)
   end
 end
